06860e733edf96c6c5d786fc18ca7edebcce7ba0
Xalvador <53310516+GrubGranny@users.noreply.github.com>
2019-08-02 09:59:59 -0700

    Delete SimpleArrays.h

--- SimpleArrays.h
+++
@@ -1,6 +0,0 @@
-void PrintArrayFloat(float array[], int arraySize);
-void PrintArrayInt(int array[], int arraySize);
-void AddArraysFloat(float array1[], float array2[], int arraySize);
-void SetArray(int array[], int arraySize, int value);
-int CompareArray(int array1[], int array2[], int arraySize);
-int DotProduct(float array1[], float array2[], int arraySize);
